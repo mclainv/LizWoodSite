ec801daf105d4db5fb1aebeb03e0b6dd8dc1cfb4
mclainv <rma9540@nyu.edu>
2025-05-01 08:13:03 -0400

    tried removing unnecessary parts of getPositions netlify function, which currently is broken on prod

--- lizwood-client/netlify/functions/getPositions.js
+++ lizwood-client/netlify/functions/getPositions.js
@@ -1,23 +1,23 @@
 // Add imports for Mongoose and dotenv
 const mongoose = require('mongoose');
-require('dotenv').config();
+// require('dotenv').config();
 // cache the Mongoose connection and models across invocations
 let isConnected = false;
 // define a reusable schema for dynamic Position models
-const PositionSchema = new mongoose.Schema({
-  path: String,
-  alt: String,
-  ogWidth: Number,
-  ogHeight: Number,
-  defaultPosition: {
-    x: Number,
-    y: Number,
-    z: Number,
-    rotated: Number,
-    width: Number,
-    height: Number,
-  },
-});
+// const PositionSchema = new mongoose.Schema({
+//   path: String,
+//   alt: String,
+//   ogWidth: Number,
+//   ogHeight: Number,
+//   defaultPosition: {
+//     x: Number,
+//     y: Number,
+//     z: Number,
+//     rotated: Number,
+//     width: Number,
+//     height: Number,
+//   },
+// });
 
 module.exports.handler = async function(event, context) {
   context.callbackWaitsForEmptyEventLoop = false;
